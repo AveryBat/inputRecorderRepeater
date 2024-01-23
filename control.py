21158587df4f93ea2c93ce14e93c22a43bf29530
AveryBat <cstr1303@gmail.com>
2024-01-23 18:52:45 -0500

    initial commit 2

---
+++ control.py
@@ -0,0 +1,59 @@
+# Import Modules
+from tkinter import *
+from pynput.mouse import Controller
+import time
+import json
+
+# File containing inputs
+json_file = "inputs.json"
+
+def replay_inputs(inputs):
+    mouse = Controller()
+
+    for i, data in enumerate(inputs):
+        type = data['type']
+        x = data['x']
+        y = data['y']
+        timestamp = data['time']
+
+        if type == 'move':
+            mouse.position = (x, y)
+            print('moving to ({0}, {1})'.format(x, y))
+        elif 'pressed' in type:
+            button = type.split()[1]
+            button = type.split('.')[1]
+            mouse.press(button)
+            print('{0} at ({1}, {2})'.format(type, x, y))
+        elif 'released' in type:
+            button = type.split()[1]
+            button = type.split('.')[1]
+            print('{0} at ({1}, {2})'.format(type, x, y))
+        elif 'scrolled' in type:
+            direction = 'down' if 'down' in type else 'up'
+            mouse.scroll(0, -1) if direction == 'down' else mouse.scroll(0, 1)
+            print('Scrolling {0} at ({1}, {2})'.format(direction, x, y))
+
+        if i < len(inputs) - 1:
+            # Calculate time difference between next and current event
+            time_difference = inputs[i + 1]['time'] - timestamp
+            time.sleep(time_difference)
+
+
+def read_from_json(file_path):
+    try:
+        with open(file_path, 'r') as json_file:
+            data = json.load(json_file)
+        return data
+    except FileNotFoundError:
+        print(f"File '(file_path)' not found")
+        return None
+    except json.JSONDecodeError:
+        print(f"Error decoding JSON in file '(file_path)'")
+        return None
+    
+# Read inputs from json file
+stored_inputs = read_from_json(json_file)
+
+# Replay inputs
+if stored_inputs:
+    replay_inputs(stored_inputs)
---
+++ monitor.py
@@ -0,0 +1,94 @@
+# Import Modules
+from tkinter import *
+from pynput import mouse
+import time
+import json
+
+# Initalize listener
+listener = None
+# Global variable to track whether the listener is running
+listener_on = False
+
+# File to store inputs
+json_file = "inputs.json"
+
+# List to store inputs
+inputs = []
+
+def toggle_listener():
+    global listener, listener_on
+    if listener_on:
+        # Stop the listener
+        listener.stop()
+        listener_on = False
+        print("Stopped")
+    else:
+        # Start the listener
+        listener = mouse.Listener(
+            on_move = on_move,
+            on_click = on_click,
+            on_scroll = on_scroll)
+        listener.start()
+        listener_on = True
+        print("Started")
+
+# Writes to .json file
+def write_to_json(data):
+    with open(json_file, 'w') as opened_file:
+        json.dump(data, opened_file)
+
+# Gets timestamp for input sorting
+def get_time():
+    return time.time()
+
+# Tracks mouse movement
+def on_move(x, y):
+    if listener_on:
+        data = ({'time': get_time(), 
+                 'type': 'move',
+                 'x': x,
+                 'y': y})
+        inputs.append(data)
+        write_to_json(inputs)
+        print('moved to {0}'.format(
+            (x, y)))
+
+# Tracks mouse clicks
+def on_click(x, y, button, pressed):
+    if listener_on:
+        action = '{0} {1}'.format(
+            'pressed' if pressed else 'released',
+            button)
+        data = ({'time': get_time(), 
+                 'type': action,
+                 'x': x,
+                 'y': y})
+        inputs.append(data)
+        write_to_json(inputs)
+        print(action)
+
+# Tracks mouse scroll
+def on_scroll(x, y, dx, dy):
+    if listener_on:
+        uppies = 'down' if dy < 0 else 'up'
+        data = ({'time': get_time(), 
+                 'type': 'scrolled' + uppies,
+                  'x': x,
+                  'y': y})
+        inputs.append(data)
+        write_to_json(inputs)
+        print('scrolled {0} at {1}'.format(
+            uppies,
+            (x, y)))
+        
+
+# Create root window and give it a title
+root = Tk()
+root.title("Avery's Input Recorder & Repeater")
+
+# Create button in window to start / stop mouse listener
+btn_toggle_listener = Button(root, text="Toggle Listener", command=toggle_listener)
+btn_toggle_listener.pack(pady=10)
+ 
+# Execute Tkinter
+root.mainloop()
